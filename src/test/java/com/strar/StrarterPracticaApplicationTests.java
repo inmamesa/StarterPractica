d31e890ff816d059c5cdc58485a72e343f7a76e9
inmamesa <inmamesa98@gmail.com>
2021-12-01 11:58:44 +0100

    primer commit

---
+++ src/test/java/com/strar/StrarterPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrarterPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
